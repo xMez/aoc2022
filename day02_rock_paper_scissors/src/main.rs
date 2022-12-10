319d72dac4b1ebfcd0b52c023330942a8a181ef8
Mez <albin.soderstrom@hotmail.com>
2022-12-10 21:55:08 +0100

    Implement solver for a hand

--- day02_rock_paper_scissors/src/main.rs
+++ day02_rock_paper_scissors/src/main.rs
@@ -3,9 +3,39 @@ use std::io::{self, Error, Read, Write};
 
 use clap::Parser;
 
+pub fn solve(hand: &str) -> u16 {
+    match hand {
+        // Rock
+        "A X" => 3 + 1, // Rock v Rock
+        "A Y" => 6 + 2, // Rock v Paper
+        "A Z" => 0 + 3, // Rock v Scissors
+        // Paper
+        "B X" => 0 + 1, // Paper v Rock
+        "B Y" => 3 + 2, // Paper v Paper
+        "B Z" => 6 + 3, // Paper v Scissors
+        // Scissors
+        "C X" => 6 + 1, // Scissors v Rock
+        "C Y" => 0 + 2, // Scissors v Paper
+        "C Z" => 3 + 3, // Scissors v Scissors
+        // Empty
+        "" => 0,
+        &_ => todo!(),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_solver() {
+        let mut result = solve("A Y");
+        assert_eq!(result, 8);
+        result = solve("B X");
+        assert_eq!(result, 1);
+        result = solve("C Z");
+        assert_eq!(result, 6)
+    }
 }
 
 #[derive(Parser)]
